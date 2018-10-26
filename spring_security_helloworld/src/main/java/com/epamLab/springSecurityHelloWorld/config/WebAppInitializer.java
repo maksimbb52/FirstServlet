97f38a0104070b30cc3c77291b16e5e48deee878
Maksim_Baranov <maksim_baranov@epam.com>
2018-10-26 18:55:35 +0300

    simple spring securitu helloworld without DB Spring 4.0.5 Release Spring Boot 1.0.1 Spring Security 3.2.3 Release Maven Tomcat 8 Servlet Api 3.1 Java 1.8

---
+++ spring_security_helloworld/src/main/java/com/epamLab/springSecurityHelloWorld/config/WebAppInitializer.java
@@ -0,0 +1,25 @@
+package com.epamLab.springSecurityHelloWorld.config;
+
+//Import section ommited...
+
+import com.epamLab.springSecurityHelloWorld.config.application.WebConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] {WebConfig.class}; // We dont need any special servlet config yet.
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+
+}
